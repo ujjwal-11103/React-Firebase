8a4d565b471d33d8c4ce36711009b1dd77689a4a
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2024-01-16 19:48:30 +0530

    Sign in feature

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
-// import React from 'react'
-// import { app } from './Firebase'
-
-// import { getDatabase, ref, set } from 'firebase/database'
-
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-
-// // here through db we can access the database (acting as a reference)
-// const db = getDatabase(app)
-
-// const auth = getAuth();
-
-
-// // checking connection to database by passing some data 
-
-// export default function App() {
-//   const putData = () => {
-//     set(ref(db, "users/ujjwal"), {
-//       user: "Ujjwal",
-//       age: 21
-//     })
-//   }
-
-//   // user sign up
-//   const signup = () => {
-//     createUserWithEmailAndPassword(auth, 'ujjwwal@gmail.com', '12ww3123')
-//       .then((userCredential) => {
-//         // Signed up 
-//         const user = userCredential.user;
-//         console.log(user);
-//         // ...
-//       })
-//       .catch((error) => {
-//         // Handle registration errors
-//         console.error('Registration error:', error.message);
-//       });
-//   }
-
-
-//   return (
-//     <div style={{ textAlign: 'center' }}>
-//       <button onClick={putData}>Store Data</button><br /> <br />
-//       <h1>Hello Firebase</h1>
-//       <button onClick={signup}>Add User</button>
-//     </div>
-//   )
-// }
-
 
 import React from 'react'
 import Authentication from './Components/Authentication'
+import Sign_in from './Components/Sign_in'
 export default function App() {
   return (
     <div>
       <Authentication />
+      <Sign_in />
     </div>
   )
 }
--- src/Components/Sign_in.js
+++ src/Components/Sign_in.js
@@ -1,55 +1,43 @@
 
-
 import React, { useState } from 'react'
+import { app } from '../Firebase'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-import { app } from '../Firebase';
+export default function Sign_in() {
 
-// import { getDatabase, ref, set } from 'firebase/database'
-
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-export default function Authentication() {
+    const auth = getAuth();
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    // reference
-    const auth = getAuth();
+    const signIn = () => {
 
-    const register = () => {
-
-        createUserWithEmailAndPassword(auth, email, password)
-
+        signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                // Signed up 
+                // Signed in 
                 const user = userCredential.user;
-                console.log(user);
-                alert("Successfully Registered !")
                 // ...
+                alert("SiGNED IN")
             })
-
             .catch((error) => {
-
                 const errorCode = error.code;
                 const errorMessage = error.message;
-
-                console.log(errorCode);
-                console.log(errorMessage);
-                // ..
             });
     }
+
     return (
         <div style={{ textAlign: 'center' }}> <br />
-            <h1>Sign Up Page</h1>
+            <h1>Sign In</h1>
+
             <label htmlFor="">Email</label><br />
             <input type="email" name="" id="" value={email} onChange={(event) => { setEmail(event.target.value) }} /> <br /><br />
 
             <label htmlFor="">Password</label><br />
             <input type="password" name="" id="" value={password} onChange={(event) => { setPassword(event.target.value) }} /> <br /><br />
 
-            <button onClick={register}>Register</button>
-
-
+            <button onClick={signIn}>Sign in</button>
         </div>
     )
 }
+
+
